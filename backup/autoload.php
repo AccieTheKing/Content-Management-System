fc362e7aec6b3c1d8e799e9bba5cea495da4465d
Acdaling <acdaling@gmail.com>
2020-03-01 23:07:32 +0100

    Removed folder

--- backup/autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Autoloader, load all classes
-require_once "controllers/ViewController.php";
-require_once "utils/Router.php";
-require_once "utils/Database.php";
-require_once "views/View.php";
