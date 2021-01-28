c2066668b5d4ebff321de307c558ae37eb3daaf1
Acdaling <acdaling@gmail.com>
2021-01-28 13:29:55 +0100

    Created enums to use

---
+++ utils/Enums.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CMS\Enums;
+
+abstract class WebsiteHeader
+{
+    const snow = "SNOW";
+    const nightsky = "NIGHTSKY";
+    const bubbles = "BUBBLES";
+    const normal = "DEFAULT";
+}
