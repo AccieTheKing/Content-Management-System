837aff1d7e4d8fbdabff203872a40fd562b9b0cc
Acdaling <acdaling@gmail.com>
2021-01-28 13:30:34 +0100

    Added enum file to import
    - thanks Ramon

--- autoload.php
+++ autoload.php
@@ -1,6 +1,7 @@
 <?php
 // Autoloader, load all classes
 require_once "controllers/ViewController.php";
+require_once "utils/Enums.php";
 require_once "utils/Router.php";
 require_once "utils/Database.php";
 require_once "views/View.php";
